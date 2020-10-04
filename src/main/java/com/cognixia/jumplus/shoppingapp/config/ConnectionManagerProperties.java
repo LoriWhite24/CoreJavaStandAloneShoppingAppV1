9cc682a5351c1c8a96211c72a22de0f0ef896971
Lori White <lori.r.white.24@gmail.com>
2020-10-03 20:38:35 -0400

    Setup Database Connection

--- src/main/java/com/cognixia/jumplus/shoppingapp/config/ConnectionManagerProperties.java
+++ src/main/java/com/cognixia/jumplus/shoppingapp/config/ConnectionManagerProperties.java
@@ -1,5 +1,93 @@
 package com.cognixia.jumplus.shoppingapp.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * The database connection manager for this app.
+ * @author Lori White
+ * @version v1 (09/14/2020)
+ */
 public class ConnectionManagerProperties {
+	
+	private static Connection connection = null;
+	
+	/**
+	 * Makes a connection to a sql server that is specified in the config.properties file. 
+	 */
+	private static void makeConnection() {
+		
+		Properties props = new Properties();
+		
+		
+		  try { props.load(new
+		  FileInputStream("src/main/resources/config.properties"));
+		  
+		  } catch (IOException e) {
+		  
+		  try { props.load(new FileInputStream("./config.properties"));
+		  
+		  } catch (IOException r) {
+		  
+		  r.printStackTrace(); } }
+		 
+		
+		
+		  String url = props.getProperty("url"); 
+		  String username = props.getProperty("username"); 
+		  String password = props.getProperty("password");
+		 
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			connection = DriverManager.getConnection(url, username, password);
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+	/**
+	 * Retrieves the current sql server connection.
+	 * @return Connection - the current sql server connection
+	 */
+	public static Connection getConnection() {
+
+		if (connection == null) { // if connection not established, create it before returning it
+			makeConnection();
+		}
+
+		return connection;
+	}
+	/**
+	 * Tests the integration of this class's getConnection function with the sql server provided in config.properties.
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		Connection conn = ConnectionManagerProperties.getConnection();
+		System.out.println("Connection made");
+		
+		try {
+			conn.close();
+			System.out.println("Closed connection");
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
