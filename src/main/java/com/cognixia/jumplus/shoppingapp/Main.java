692337b850df24f8df79aaa85bdef985e96cdf20
Lori White <lori.r.white.24@gmail.com>
2020-10-05 03:17:16 -0400

    Updated the utility classes

--- src/main/java/com/cognixia/jumplus/shoppingapp/Main.java
+++ src/main/java/com/cognixia/jumplus/shoppingapp/Main.java
@@ -1,11 +1,39 @@
 package com.cognixia.jumplus.shoppingapp;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+import com.cognixia.jumplus.shoppingapp.model.Customer;
+import com.cognixia.jumplus.shoppingapp.model.Invoice;
+import com.cognixia.jumplus.shoppingapp.controller.AddressController;
+import com.cognixia.jumplus.shoppingapp.controller.CustomerController;
+import com.cognixia.jumplus.shoppingapp.controller.InvoiceController;
+import com.cognixia.jumplus.shoppingapp.controller.InvoiceProductController;
+import com.cognixia.jumplus.shoppingapp.controller.ProductController;
+import com.cognixia.jumplus.shoppingapp.dao.AddressDAO;
+import com.cognixia.jumplus.shoppingapp.dao.CustomerDAO;
+import com.cognixia.jumplus.shoppingapp.dao.InvoiceDAO;
+import com.cognixia.jumplus.shoppingapp.dao.InvoiceProductDAO;
+import com.cognixia.jumplus.shoppingapp.dao.ProductDAO;
+
 /**
  * Core Java Stand Alone Shopping Application version 1 main functionality.
  * @author Lori White
  * @version v1 (10/03/2020)
  */
 public class Main {
+	private static AddressDAO addressRepo = new AddressController();
+	private static CustomerDAO customerRepo = new CustomerController();
+	private static InvoiceDAO invoiceRepo = new InvoiceController();
+	private static InvoiceProductDAO invoiceProductRepo = new InvoiceProductController();
+	private static ProductDAO productRepo = new ProductController();
+	
+	private static Scanner in = new Scanner(System.in);
+	private static Customer currentCustomer = null;
+	private static Invoice currentInvoice = null;
+	
+	private static DecimalFormat df = new DecimalFormat("#,###,##0.00");
+	private static final int minChoice = 1;
 	/**
 	 * The second entry point for this App.
 	 * @param args the command line arguments
--- src/main/java/com/cognixia/jumplus/shoppingapp/utility/ConsolePrinterUtility.java
+++ src/main/java/com/cognixia/jumplus/shoppingapp/utility/ConsolePrinterUtility.java
@@ -1,5 +1,99 @@
 package com.cognixia.jumplus.shoppingapp.utility;
 
-public class ConsolePrinterUtility {
+import java.util.List;
 
+/**
+ * The utility for storing reusable output formating for this app.
+ * @author Lori White
+ * @version v1 (10/05/2020)
+ */
+public class ConsolePrinterUtility {
+	/**
+	 * Generates the header, colors, and then prints it to the header.
+	 * @param choice the header type
+	 */
+	public static void header(String choice) {
+		String header = "";
+		
+		switch(choice) {
+		case "top":
+			header = "+----------------+\n| Welcome Guest! |\n+----------------+";
+			break;
+		case "logged_in":
+			header = "+-------------------+\n| Welcome Customer! |\n+-------------------+";
+			break;
+		case "register":
+			header = "+---------------------------+\n| Enter Details to Register |\n+---------------------------+";
+			break;
+		case "purchase":
+			header = "+------------------------------+\n| Items Available for Purchase |\n+------------------------------+";
+			break;
+		case "login":
+			header = "+---------------------+\n| Enter Login Details |\n+---------------------+";
+			break;
+		case "customer":
+			header = "+------------------+\n| Customer Details |\n+------------------+";
+			break;
+		case "customer_update":
+			header = "+-------------------------+\n| Update Customer Details |\n+-------------------------+";
+			break;
+		case "invoices":
+			header = "+---------------+\n| Your Invoices |\n+---------------+";
+			break;
+		case "invoice":
+			header = "+------------------+\n| Choose an Action |\n+------------------+";
+			break;
+		case "return":
+			header = "+----------------+\n| Making a Return |\n+----------------+";
+			break;
+		}
+		ColorsUtility.colorHeader(header);
+	}
+	/**
+	 * Generates the menuChoice, colors, and then prints it to the menuChoice.
+	 * @param numChoice the number of choices
+	 */
+	public static void menuChoice(int numChoice) {
+		String menuChoice = "Enter Choice (";
+		
+		for(int i = 1; i <= numChoice; i++) {
+			if(i == numChoice) {
+				menuChoice += "or " + i + "):";
+			} else {
+				menuChoice += i + ", ";
+			}
+		}
+		ColorsUtility.colorChoice(menuChoice);
+	}
+	/**
+	 * Generates the menu, colors, and then prints it to the menu.
+	 * @param choices the menu of choices to print
+	 */
+	public static void menu(List<String> choices) {
+		String menu = "";
+		for(int i = 1; i <= choices.size(); i++) {
+			menu += i + ". " + choices.get(i - 1);
+			if(i != choices.size()) {
+				menu += "\n";
+			}
+		}
+		ColorsUtility.colorMenu(menu);
+	}
+	/**
+	 * Generates the error, colors, and then prints it to the error.
+	 * @param choice the error type
+	 */
+	public static void error(String choice) {
+		String error = "";
+		
+		switch(choice) {
+		case "login":
+			error = "Invalid Credentials. Please try again!";
+			break;
+		case "input":
+			error = "Invalid Input. Please try again!";
+			break;
+		}
+		ColorsUtility.colorError(error);
+	}
 }
